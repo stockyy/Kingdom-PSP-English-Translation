4e4fd8046afdff42d5dec2064ad642a2f66998ec
stockyy <fnnb5132@leeds.ac.uk>
2026-05-24 13:24:59 +0100

    directory file header completed

--- injection.py
+++ injection.py
@@ -1,20 +1,22 @@
 import os
 import struct
 
+INFILES = ["DAT", "MSN", "UI"]
+
 def read_headers(directory):
     print(f"--- Getting header details for the files in {directory} ---")
 
     for file in os.scandir(directory):
         if file.is_file():
-            with open(file, "r") as fp:
+            with open(file, "rb") as fp:
                 header_data = fp.read(16)
                 magic_number, string_count, pointer_table_offset, string_data_offset = struct.unpack("<IIII", header_data)
 
-                print(f"\n-- {file}--")
-                print(f"\tFirst 4 Bytes: {magic_number}")
-                print(f"\tSecond 4 Bytes: {string_count}")
-                print(f"\tThird 4 Bytes: {pointer_table_offset}")
-                print(f"\tFourth 4 Bytes: {string_data_offset}\n")
+                print(f"\t-- Headers for: {file}--")
+                print(f"\t\tFirst 4 Bytes: {magic_number}")
+                print(f"\t\tSecond 4 Bytes: {string_count}")
+                print(f"\t\tThird 4 Bytes: {pointer_table_offset}")
+                print(f"\t\tFourth 4 Bytes: {string_data_offset}\n")
 
 
 if __name__ == "__main__":
@@ -22,7 +24,7 @@ if __name__ == "__main__":
     if not os.path.exists("target_files/"):
         print("Cannot find binary files ")
 
-    read_headers("target files/")
-    read_headers("extracted_iso/USRDIR/")
+    for file in INFILES:
+        read_headers(f"extracted_iso/{file}/")
 
         
